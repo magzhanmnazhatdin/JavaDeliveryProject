9f994ff3776ec9481b86f7370f01af3c3d70d10a
magzhanmnazhatdin <79352934+magzhanmnazhatdin@users.noreply.github.com>
2025-11-23 21:50:48 +0500

    created project

---
+++ src/main/java/com/example/deliverysevice/DeliverySeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliverysevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverySeviceApplication.class, args);
+    }
+
+}
